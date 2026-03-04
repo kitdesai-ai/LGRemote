5a095ad7b034bb1ad105965e6bd81a2457ba68b5
Ankit Desai <ankit@pocketlabs.io>
2026-03-03 22:30:46 -0500

    Initial Commit

---
+++ LGRemote/LGRemoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  LGRemoteApp.swift
+//  LGRemote
+//
+//  Created by Ankit Desai on 3/3/26.
+//
+
+import SwiftUI
+
+@main
+struct LGRemoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
